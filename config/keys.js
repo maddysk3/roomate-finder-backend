92541426a6753127b0f9e4a46d4dac77c7ba8d20
maddysk3 <53914850+maddysk3@users.noreply.github.com>
2022-11-06 15:44:40 -0500

    masterCommit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://maddysk01:qwerty1234@reactapp.ytzshv5.mongodb.net/test"
+};
---
+++ config/passport.js
@@ -0,0 +1,26 @@
+module.exports = {
+    mongoURI: "mongodb+srv://maddysk01:qwerty1234@reactapp.ytzshv5.mongodb.net/test",
+    secretOrKey: "secret"
+};
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../config/keys");
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+module.exports = passport => {
+    passport.use(
+        new JwtStrategy(opts, (jwt_payload, done) => {
+            User.findById(jwt_payload.id)
+                .then(user => {
+                    if (user) {
+                        return done(null, user);
+                    }
+                    return done(null, false);
+                })
+                .catch(err => console.log(err));
+        })
+    );
+};
